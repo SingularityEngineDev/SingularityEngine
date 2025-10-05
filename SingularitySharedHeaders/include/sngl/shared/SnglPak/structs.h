8e373c602a7d0417a001feb73bea2d94ec54efd6
YasInvolved <jbaczyk27@gmail.com>
2025-10-06 00:39:31 +0200

    pack archive structures

--- SingularitySharedHeaders/include/sngl/shared/SnglPak/structs.h
+++ SingularitySharedHeaders/include/sngl/shared/SnglPak/structs.h
@@ -1,12 +1,22 @@
 #ifndef _SNGL_SHARED_SNGLPAK_STRUCTS_H_INCLUDED_
 #define _SNGL_SHARED_SNGLPAK_STRUCTS_H_INCLUDED_
 
+#include <sngl/shared/definitions.h>
+
 namespace sngl::shared::sngl_pak
 {
+	SNGL_BEGIN_PACK
 	struct TocEntry
 	{
-		inline TocEntry(const std::string& filename)
-	};
+		inline TocEntry(const std::string& filename, size_t _fileSize, size_t _blockIndex)
+			: filenameLength(filename.size()), fileSize(_fileSize), blockIndex(_blockIndex)
+		{ }
+
+		size_t filenameLength;
+		size_t fileSize;
+		size_t blockIndex;
+	} SNGL_PACK;
+	SNGL_END_PACK
 }
 
 #endif
--- SingularitySharedHeaders/include/sngl/shared/definitions.h
+++ SingularitySharedHeaders/include/sngl/shared/definitions.h
@@ -11,4 +11,16 @@
 	#define SNGL_API
 #endif
 
+#ifdef _MSC_VER
+	#define SNGL_BEGIN_PACK __pragma(pack(push, 1))
+	#define SNGL_END_PACK __pragma(pack(pop))
+	#define SNGL_PACK
+#elif defined(__GNUC__) || defined(__clang__)
+	#define SNGL_BEGIN_PACK
+	#define SNGL_END_PACK
+	#define SNGL_PACK __attribute__((packed))
+#else
+	#error "Unsupported compiler, please define packing macros"
+#endif
+
 #endif
