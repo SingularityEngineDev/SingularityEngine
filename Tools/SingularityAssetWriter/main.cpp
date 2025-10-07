98797fc02fe97753e9f13589fb5f1be0449e6e86
YasInvolved <jbaczyk27@gmail.com>
2025-10-07 23:12:26 +0200

    rewrite asset writer

--- Tools/SingularityAssetWriter/main.cpp
+++ Tools/SingularityAssetWriter/main.cpp
@@ -3,29 +3,127 @@
 
 namespace fs = std::filesystem;
 
-// type aliases
-using TocEntry = sngl::shared::sngl_pak::TocEntry;
-
-// extending types
-SNGL_BEGIN_PACK
-struct ExtendedTocEntry : TocEntry
+class Application
 {
-	inline ExtendedTocEntry(const std::string& _filename, size_t filesize, size_t blockIndex, const std::string& _path)
-		: TocEntry(_filename, filesize, blockIndex), filename(_filename), path(_path)
-	{ }
+private:
+	// local type aliases
+	using TocEntry = sngl::shared::sngl_pak::TocEntry;
+	using IFile = sngl::io::IFile;
 
-	std::string filename;
-	std::string path;
-} SNGL_PACK;
-SNGL_END_PACK
+	// typedefs
+	SNGL_BEGIN_PACK
+		struct StorageTocEntry : TocEntry
+	{
+		inline StorageTocEntry(const std::string& archivePath, size_t filesize, size_t blockIndex, const std::string& _path)
+			: TocEntry(archivePath, filesize, blockIndex), path(_path)
+		{
+		}
 
-// constants
-constexpr size_t BLOCK_SIZE_THRESHOLD = (1 << 18); // 256kb
+		std::string path;
+	} SNGL_PACK;
+	SNGL_END_PACK
+
+	// constants
+	static constexpr size_t BLOCK_SIZE_THRESHOLD = (1 << 18);
+
+	// data
+	std::string m_outputPath;
+	std::vector<std::string> m_filesToInclude;
+	std::vector<StorageTocEntry> m_tocEntries;
+	std::ofstream m_outputFile;
+	std::vector<char> m_buffer;
+	size_t m_currentBlockIndex;
+	size_t m_currentBlockSize;
+
+public:
+	Application(std::string&& outputPath, std::vector<std::string>&& filesToInclude)
+		: m_outputPath(std::move(outputPath)), 
+		m_filesToInclude(std::move(filesToInclude)), 
+		m_outputFile(m_outputPath, std::ios::binary | std::ios::trunc),
+		m_buffer(BLOCK_SIZE_THRESHOLD)
+	{
+		m_tocEntries.reserve(m_filesToInclude.size());
+	}
+
+	void run()
+	{
+		for (const auto& path : m_filesToInclude)
+		{
+			fmt::println("Processing {}", path);
+
+			if (!fs::exists(path))
+			{
+				fmt::println("Skipping {}. File doesn't exist.", path);
+				continue;
+			}
+
+			if (!fs::is_directory(path))
+			{
+				addFile(path);
+				continue;
+			}
+		}
+
+		for (const auto& entry : m_tocEntries)
+		{
+			TocEntry toc = static_cast<TocEntry>(entry);
+			m_outputFile.write(reinterpret_cast<const char*>(&toc), sizeof(TocEntry));
+		}
+
+		{
+			size_t entries = m_tocEntries.size();
+			m_outputFile.write(reinterpret_cast<const char*>(&entries), sizeof(size_t));
+		}
+	}
+
+private:
+	// for standalone files
+	void addFile(const std::string& path)
+	{
+		fs::path p(path);
+		assert(!fs::is_directory(p));
+		assert(p.has_filename());
+
+		addToToc(p.filename().generic_string(), path);
+
+	}
+
+	// for files inside directories
+	void addDirectoryFile(const std::string& path, const std::string& root)
+	{
+		assert(!fs::is_directory(path));
+		
+		std::string archivePath = fs::relative(path, root).generic_string();
+		addToToc(archivePath, path);
+	}
+
+	void addToToc(const std::string& archivePath, const std::string& filePath)
+	{
+		auto file = IFile::Open(filePath, IFile::IoType::IT_MAPPED);
+		size_t filesize = file->getSize();
+		m_tocEntries.emplace_back(archivePath, filesize, m_currentBlockSize, filePath);
+		
+		// write file contents to archive, TOC is written at the end
+		while (true)
+		{
+			size_t readSize = file->readSync(m_buffer.data(), m_buffer.size());
+			if (readSize == 0)
+				break;
+
+			m_outputFile.write(m_buffer.data(), readSize);
+		}
+
+		m_currentBlockSize += filesize;
+		if (m_currentBlockSize >= BLOCK_SIZE_THRESHOLD)
+		{
+			m_currentBlockSize = 0;
+			m_currentBlockIndex++;
+		}
+	}
+};
 
 int main(int argc, char** argv)
 {
-	using IFile = sngl::io::IFile;
-
 	// parse arguments
 	argparse::ArgumentParser program("SingularityAssetWriter", "1.0");
 	program.add_argument("-o", "--output")
@@ -44,7 +142,7 @@ int main(int argc, char** argv)
 	}
 	catch (const std::runtime_error& e)
 	{
-		fmt::println("{}\n\n{}", e.what(), program.print_help());
+		fmt::println("{}\n\n{}", e.what(), program.help().str());
 		return -1;
 	}
 
@@ -53,103 +151,15 @@ int main(int argc, char** argv)
 
 	fmt::print("Writing assets to: {}\n", outputPath);
 
+	Application app(std::move(outputPath), std::move(filesToInclude));
+
 	try
 	{
-		std::vector<ExtendedTocEntry> tocEntries;
-		tocEntries.reserve(filesToInclude.size()); // reduce allocations count
-
-		std::ofstream outFile(outputPath, std::ios::binary | std::ios::trunc);
-		std::vector<uint8_t> buffer(BLOCK_SIZE_THRESHOLD);
-
-		size_t blockIndex = 0;
-		{
-			size_t uncompressedBlockSize = 0;
-			auto addToToc = [&blockIndex, &uncompressedBlockSize, &tocEntries](const std::string& path)
-				{
-					assert(!fs::is_directory(path));
-
-					size_t filesize = IFile::GetSize(path);
-					std::string filename = fs::path(path).filename().generic_string();
-
-					uncompressedBlockSize += filesize;
-					tocEntries.emplace_back(std::move(filename), filesize, blockIndex, path);
-
-					if (uncompressedBlockSize > BLOCK_SIZE_THRESHOLD)
-						blockIndex++;
-				};
-
-			auto addToTocDir = [&blockIndex, &uncompressedBlockSize, &tocEntries](const std::string& path, const std::string& rootDir)
-				{
-					assert(!fs::is_directory(path));
-
-					size_t filesize = IFile::GetSize(path);
-					std::string filename = fs::relative(path, rootDir).generic_string();
-
-					uncompressedBlockSize += filesize;
-					tocEntries.emplace_back(std::move(filename), filesize, blockIndex, path);
-
-					if (uncompressedBlockSize > BLOCK_SIZE_THRESHOLD)
-						blockIndex++;
-				};
-
-			auto writeFileToArchive = [&outFile, &buffer](const std::string& path)
-				{
-					auto file = IFile::Open(path, IFile::IoType::IT_MAPPED);
-
-					size_t readSize = 0;
-					while (true)
-					{
-						readSize = file->readSync(buffer.data(), buffer.size());
-						if (readSize == 0)
-							break;
-
-						outFile.write((const char*)buffer.data(), readSize);
-					}
-				};
-
-			for (const auto& path : filesToInclude)
-			{
-				fmt::println("Processing {}", path);
-
-				if (not fs::exists(path))
-				{
-					fmt::println("Failed to process {}. File or directory doesn't exist", path);
-					continue;
-				}
-
-				if (not fs::is_directory(path))
-				{
-					addToToc(path);
-					writeFileToArchive(path);
-				}
-
-				std::string relativeTo = fs::path(path).parent_path().generic_string();
-				for (const auto& entry : fs::recursive_directory_iterator(path))
-				{
-					const fs::path& entryPath = entry.path();
-					const std::string entryPathStr = entryPath.generic_string();
-
-					if (entry.is_directory())
-						continue;
-
-					fmt::println("Processing {}", entryPathStr);
-					addToTocDir(entryPathStr, relativeTo);
-					writeFileToArchive(entryPathStr);
-				}
-			}
-
-			for (const auto& extendedTocEntry : tocEntries)
-			{
-				const auto& filename = extendedTocEntry.filename;
-				const TocEntry& tocEntry = static_cast<const TocEntry&>(extendedTocEntry);
-				outFile.write((const char*)&tocEntry, sizeof(TocEntry));
-				outFile.write(filename.data(), filename.size());
-			}
-		}
+		app.run();
 	}
 	catch (const std::runtime_error& e)
 	{
-		fmt::println("Critical error: {}", e.what());
+		fmt::println("Application error: {}", e.what());
 		return -1;
 	}
 
