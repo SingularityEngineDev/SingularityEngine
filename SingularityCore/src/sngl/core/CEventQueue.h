c4ba5b49b963f87b80948db6b6a7c73170019b72
YasInvolved <jbaczyk27@gmail.com>
2025-10-14 22:56:18 +0200

    synchronize event queue

--- SingularityCore/src/sngl/core/CEventQueue.h
+++ SingularityCore/src/sngl/core/CEventQueue.h
@@ -19,33 +19,37 @@
 
 namespace sngl::core
 {
-   class CEventQueue
-   {
-   private:
-      using EventPtr = std::unique_ptr<IEvent>;
+    class CEventQueue
+    {
+    private:
+            using EventPtr = std::unique_ptr<IEvent>;
 
-   public:
-      inline void push(std::unique_ptr<IEvent> e)
-      {
-         m_events.emplace_back(std::move(e));
-      }
+        public:
+            inline void push(std::unique_ptr<IEvent> e)
+            {
+                std::lock_guard<std::mutex> lock(m_queueMutex);
+                m_events.emplace_back(std::move(e));
+            }
 
-      inline bool pop(std::unique_ptr<IEvent>& e) 
-      {
-         if (m_events.empty()) return false;
-         e = std::move(m_events.front());
-         m_events.pop_front();
-         return true;
-      }
+            inline bool pop(std::unique_ptr<IEvent>& e) 
+            {
+                std::lock_guard<std::mutex> lock(m_queueMutex);
+                if (m_events.empty()) return false;
+                e = std::move(m_events.front());
+                m_events.pop_front();
+                return true;
+            }
 
-      inline void clear()
-      {
-         m_events.clear();
-      }
+            inline void clear()
+            {
+                std::lock_guard<std::mutex> lock(m_queueMutex);
+                m_events.clear();
+            }
       
-   private:
-      std::deque<EventPtr> m_events;
-   };
+    private:
+        std::mutex m_queueMutex;
+        std::deque<EventPtr> m_events;
+    };
 }
 
 #endif
