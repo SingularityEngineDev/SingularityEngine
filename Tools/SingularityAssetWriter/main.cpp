e9f2ab5a1320009bff873d315786ee7564aa3b35
YasInvolved <jbaczyk27@gmail.com>
2025-10-20 14:13:19 +0200

    move header structures to the shared header

--- Tools/SingularityAssetWriter/main.cpp
+++ Tools/SingularityAssetWriter/main.cpp
@@ -1,37 +1,7 @@
 namespace fs = std::filesystem;
 
-struct alignas(8) ArchiveHeader
-{
-	static constexpr uint64_t DEFAULT_BLOCKSIZE = (1 << 14);
-	static constexpr uint32_t MAGIC_VALUE = 0x4B415053;
-	static constexpr uint16_t CURRENT_VERSION = 0x0010;
-
-	ArchiveHeader()
-		: magic(MAGIC_VALUE), 
-		version(CURRENT_VERSION),
-		blockSize(DEFAULT_BLOCKSIZE),
-		filetableOffset(sizeof(ArchiveHeader))
-	{ }
-
-	uint32_t magic;
-	uint16_t version;
-	uint16_t reserved;
-	uint64_t blockSize;
-	uint64_t filecount;
-	uint64_t filetableOffset;
-	uint64_t dataOffset;
-};
-
-struct alignas(8) FileEntry
-{
-	char path[256];
-	uint64_t blockCount;
-	uint64_t startBlockOffset;
-	uint64_t startBlockDataOffset;
-};
-
-static_assert(sizeof(ArchiveHeader) == 40);
-static_assert(sizeof(FileEntry) == 280);
+using ArchiveHeader = sngl::shared::spak::ArchiveHeader;
+using FileEntry = sngl::shared::spak::FileEntry;
 
 class Application
 {
